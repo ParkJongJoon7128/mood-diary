dbc60d9c781b266730379ae1e800e74e76797a12
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-02-01 23:06:00 +0900

    일기 중복 추가 예외 처리 구현 완료

--- src/components/CalendarSelectDayComponent.tsx
+++ src/components/CalendarSelectDayComponent.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { DateData } from 'react-native-calendars';
 import { DayState } from 'react-native-calendars/src/types';
+import Snackbar from 'react-native-snackbar';
+import { colors } from 'react-native-tailwindcss';
 import { useRecoilValue } from 'recoil';
 import { totalDate } from '../data/common';
 import { diaryListState } from '../data/dataState';
@@ -29,10 +31,28 @@ const CalendarSelectDayComponent: FC<CalendarSelectDayComponentProps> = ({
   );
   const isSelected = !!existDate;
 
+  const showToast = () => {
+    Snackbar.show({
+      text: '해당 날짜에는 일기가 존재해 입력이 불가능 합니다.',
+      duration: Snackbar.LENGTH_SHORT,
+      backgroundColor: 'white',
+      textColor: colors.black,
+      action: {
+        text: '닫기',
+        textColor: 'red',
+        onPress: () => { Snackbar.dismiss() },
+      },
+    })
+  };
+
+  const addDiary = (date: DateData | undefined) => {
+    isSelected ? showToast() : handleChange(date);
+  };
+
   //View
   return (
     <View className="flex p-2">
-      <TouchableOpacity onPress={() => handleChange(date)}>
+      <TouchableOpacity onPress={() => addDiary(date)}>
         <Text
           className={`text-black ${isSelected ? 'opacity-20' : 'opacity-100'} ${
             state === 'today' ? 'text-blue-500' : 'text-black'
