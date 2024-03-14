b4fe304b5cc702983d6ed11142609b3ca894a24a
parkjongjoon7128 <whdwnsdk8111@naver.com>
2024-03-15 00:36:46 +0900

    안드로이드 패키지명 수정

--- android/app/src/main/java/com/mooddiaryapp/MainActivity.kt
+++ android/app/src/main/java/com/mooddiaryapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mooddiaryapp
+package com.MoodDiaryApp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/mooddiaryapp/MainApplication.kt
+++ android/app/src/main/java/com/mooddiaryapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mooddiaryapp
+package com.MoodDiaryApp
 
 import android.app.Application
 import com.facebook.react.PackageList
